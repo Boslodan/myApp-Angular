582282be1115ac6ad32dfa5d299abf8f5ac70b9c
Boslodan <sjankovic01@gmail.com>
2024-03-14 23:16:38 +0100

    Html,Css-Header i Body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { BodyComponent } from './Components/body/body.component';
+import { FormTaskComponent } from './Components/form-task/form-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavBarComponent,
+    BodyComponent,
+    FormTaskComponent
   ],
   imports: [
     BrowserModule,
